c21c765abddcc944636a4fbdd58f4a79a0976829
Robin Bron <robin@finwo.nl>
2018-10-19 17:07:44 +0200

    Setup test env

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Save for later
+OWD=$(realpath $(dirname $0));
+
+echo "Updating .gitignore"
+curl -s https://www.gitignore.io/api/osx,linux,windows,node > .gitignore
+echo ".idea/" >> .gitignore
+echo ".gtm/" >> .gitignore
+
+#echo "Updating modules"
+#git submodule foreach --recursive git clean -xfd &>/dev/null
+#git submodule update --init --recursive
