599f9f9b261711ecb66ffce4a9c5fb665f08a2f3
Robin Bron <finwo@pm.me>
2021-07-24 03:19:06 +0200

    Fixed inconsistent directory detection

--- src/to-absolute-path.js
+++ src/to-absolute-path.js
@@ -5,9 +5,9 @@ module.exports = function toAbsolutePath( parsedLocation ) {
   if ( 'string' === typeof parsedLocation ) {
     parsedLocation = require('url-parse')(parsedLocation);
   }
-  let targetLocation = parsedLocation.hostname + parsedLocation.pathname;
-  if ( targetLocation.substr(0,1) !== '/' ) {
-    targetLocation = basedir + '/' + targetLocation;
+  let targetLocation = '/' + parsedLocation.hostname + parsedLocation.pathname;
+  if (!( parsedLocation.protocol && parsedLocation.hostname )) {
+    targetLocation = basedir + targetLocation;
   }
   return targetLocation.replace(/\//g,path.sep);
 };
